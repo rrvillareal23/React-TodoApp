90043ff44361dc85f2fbae47fb738c3cefab0367
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-02-24 08:23:07 -0800

    Added complete, edit and delete functions

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,14 +3,19 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { EditTodoForm } from './EditTodoForm'
 
-export const Todo = () => {
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
   return (
     <div className='Todo'>
-        <p> Go to School</p>
+        <p onClick={()=> toggleComplete(task.id)} className={`${task.completed ? 'completed':""}`}> {task.task}</p>
         <div>
-            <FontAwesomeIcon icon={faPenToSquare} />
-            <FontAwesomeIcon icon={faTrash} />
+            <FontAwesomeIcon icon={faPenToSquare} 
+                onClick={() => editTodo(task.id)}
+            />
+            <FontAwesomeIcon icon={faTrash} 
+                onClick={() => deleteTodo(task.id)}
+            />
         </div>
     </div>
   )
