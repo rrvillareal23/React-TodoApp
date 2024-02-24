9c57edc288ddd20a323cde50913c5953b472d188
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-02-24 07:56:58 -0800

    Update so task deletes from tast bar after submit.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const TodoForm = () => {
-  return (
-    <form className = 'TodoForm'>
-        <input type='text' className='todo-input' placeholder='What is the tast today?'></input>
-        <button type='submit' className='todo-btn'> Add Task</button>
-    </form>
-  )
-}
+export const TodoForm = ({ addTodo }) => { // Fixed the prop name here
+    const [value, setValue] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        addTodo(value);
+        setValue("")
+    };
+
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type='text'
+                className='todo-input'
+                value = {value} 
+                placeholder='What is the task today?'
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button type='submit' className='todo-btn'>Add Task</button>
+        </form>
+    );
+};
